aa0ae698df2399af1392d6b08efe9ddc45762532
Luis Hinostroza <lehinostroza@hotmail.com>
2019-08-21 19:27:06 -0500

    Add project files.

---
+++ SoccerTournamentLibrary/PrizeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerTournamentLibrary
+{
+    public class PrizeModel
+    {
+        public int PLaceNumber { get; set; }
+        public string PLaceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+
+    }
+}
---
+++ SoccerTournamentLibrary/TournamentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerTournamentLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public string EntryFee { get; set; }
+        public string EnteredTeams { get; set; } = new List<TeamModel>();
+        public string Prizes { get; set; } = new List<PrizeModel>();
+        public string Rounds { get; set; } = new List<List<MatchupModel>>();
+
+    }
+}
